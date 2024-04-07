b02a93c9637c2936ff2819e7667ac286d7f3ade1
Yannick GALLY <yannick.gally@webedia-group.com>
2024-04-07 10:46:35 +0200

    feat: add footer ^^

--- src/sample.js
+++ src/sample.js
@@ -4,7 +4,7 @@ const port = 3000
 
 app.get('/', (req, res) => {
   console.log("Received request \\o/")
-  res.send('Hello Sample Express!')
+  res.send('Hello Sample Express!<br/><br/><br/>[(c) Yannick GALLY 2024 - MIT license (^_^x)]')
 })
 
 app.listen(port, () => {
