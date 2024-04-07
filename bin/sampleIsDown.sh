9cd5f0443b5d93148d5faf7b27f33e19d74e554a
Yannick GALLY <yannick.gally@webedia-group.com>
2024-04-07 13:19:23 +0200

    test: upgrade test scripts

--- bin/sampleIsDown.sh
+++ bin/sampleIsDown.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+NC="\\033[0;39m"
+GREEN="\\033[1;32m"
+RED="\\033[1;31m"
+BLUE="\\033[1;36m"
+
 {
 	curl --no-progress-meter --head localhost:3000 2>&1 \
 		|| echo 'OFF'
-} | grep -q 'OFF'
+} | grep -q 'OFF' \
+	|| {
+		echo >&2 -e "${RED}Site seems not to refuse connection${NC}"
+		false
+	}
 
 exit
--- bin/sampleIsUp.sh
+++ bin/sampleIsUp.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+NC="\\033[0;39m"
+GREEN="\\033[1;32m"
+RED="\\033[1;31m"
+BLUE="\\033[1;36m"
+
 curl --silent --head localhost:3000 \
-	| head -1 \
-	| grep -q '200 OK'
+		| head -1 \
+		| grep -q '200 OK' \
+|| {
+	echo >&2 -e "${RED}Site does not respond with HTTP 200 status code${NC}"
+	false
+}
 
 exit
